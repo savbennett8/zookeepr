aa9a09c8b4417d1ea6244d9be98cb0f6fe9a6eab
Savannah Bennett <savvy.bennett8@gmail.com>
2021-04-01 18:44:22 -0600

    animal routes moved from server.js to animalRoutes.js

--- server.js
+++ server.js
@@ -25,44 +25,6 @@ app.use(express.static('public'));
 
 // -------------------------------------//
 
-//adds the route that the front-end can request data from
-//get(route the client will fetch from, callback function to execute each time this route is accessed w/GET request)
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-//a param route must come after the other GET route
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-//this creates a route that listens for POST requests for the server to accept data
-app.post('/api/animals', (req, res) => {
-    //req.body is where our incoming content will be
-    //set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    //if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-        //add animal to json file and animals array in this function
-        const animal = createNewAnimal(req.body, animals);
-
-        //sends the info directly back to the client - use only for testing endpoints
-        res.json(animal);
-    }
-});
-
 // '/' is the route (to the root of the server) used to create a homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
